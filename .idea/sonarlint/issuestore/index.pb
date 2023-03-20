22b4abdc58993ffd99c36561ab4c6ec1bb4ead03
Vincent Kohm <vini.kohm@gmx.de>
2023-03-21 00:06:41 +0100

    Started project with basic script

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+9
+	script.py,d/b/db3114d7223bc9bf7f8dd902c5a35833f8ffdf82
---
+++ script.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import openai
+
+def get_api_key(var):
+    key = os.environ.get(var)
+    if key is None:
+        raise ValueError("Environment variable not set")
+    return key
+
+openai.api_key = get_api_key("OPENAI_KEY")
+
+model_engine = "text-davinci-003"
+temperature = 0.5
+max_tokens = 200
+
+while True:
+
+    user_input = input("> ")
+    if user_input == "q":
+        break
+
+    response = openai.Completion.create(
+        engine = model_engine,
+        prompt = user_input.strip(),
+        temperature = temperature,
+        max_tokens = max_tokens,
+        n = 1,
+        stop = None,
+        frequency_penalty = 0,
+        presence_penalty = 0
+    )
+
+    generated_text = response.choices[0].text.strip()
+    print(generated_text)
+
+sys.exit()
