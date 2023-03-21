90d874b758d2b7407db402385fa858a5f5bcd588
Vincent Kohm <vini.kohm@gmx.de>
2023-03-21 12:35:02 +0100

    Added program arguments for single response use and altered model temperature

--- script.py
+++ script.py
@@ -9,21 +9,23 @@ def get_api_key(var):
         raise ValueError("Environment variable not set")
     return key
 
+def concatenate_strings(strings):
+    strings = strings[1:]
+    
+    concatenated_string = " ".join(strings)
+
+    return concatenated_string
+
 openai.api_key = get_api_key("OPENAI_KEY")
 
 model_engine = "text-davinci-003"
-temperature = 0.5
+temperature = 0.7
 max_tokens = 200
 
-while True:
-
-    user_input = input("> ")
-    if user_input == "q":
-        break
-
+def api_call(user_input):
     response = openai.Completion.create(
         engine = model_engine,
-        prompt = user_input.strip(),
+        prompt = user_input,
         temperature = temperature,
         max_tokens = max_tokens,
         n = 1,
@@ -32,7 +34,25 @@ while True:
         presence_penalty = 0
     )
 
-    generated_text = response.choices[0].text.strip()
+    return response.choices[0].text.strip()
+
+
+if (len(sys.argv) > 1):
+    prompt = concatenate_strings(sys.argv)
+    generated_text = api_call(prompt)
+    print(generated_text)
+    pyperclip.copy(generated_text)
+    sys.exit()
+
+
+while True:
+
+    user_input = input("> ")
+    if user_input == "q":
+        break
+
+    generated_text = api_call(user_input.strip())
+
     print(generated_text)
     pyperclip.copy(generated_text)
 
